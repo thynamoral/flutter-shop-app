704489b37a0baf21d31f6b3f519740ad2b898d75
Namoral Thy <thynamoral18@gmail.com>
2025-05-21 15:19:40 +0700

    add product_constant

---
+++ lib/constants/product_constant.dart
@@ -0,0 +1,36 @@
+class ProductConstant {
+  static final productList = [
+    {
+      'id': '0',
+      'title': 'Men\'s Nike Shoes',
+      'price': 44.52,
+      'imageUrl': 'assets/images/Shoes_1.png',
+      'company': 'Nike',
+      'sizes': [9, 10, 11, 12],
+    },
+    {
+      'id': '1',
+      'title': 'Addidas Shoes',
+      'price': 20.12,
+      'imageUrl': 'assets/images/Shoes_2.png',
+      'company': 'Addidas',
+      'sizes': [9, 10, 12],
+    },
+    {
+      'id': '2',
+      'title': 'Bata Women\'s Shoes',
+      'price': 28.95,
+      'imageUrl': 'assets/images/shoes_3.png',
+      'company': 'Bata',
+      'sizes': [8, 9, 10],
+    },
+    {
+      'id': '3',
+      'title': 'Jordan Shoes',
+      'price': 420.69,
+      'imageUrl': 'assets/images/Shoes_4.png',
+      'company': 'Nike',
+      'sizes': [8, 9, 10],
+    },
+  ];
+}
